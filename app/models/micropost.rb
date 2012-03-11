5e15bc43c3ac30c899563e5aa4f329b12df7efdd
Rob Jones <robert.buckingham.jones@gmail.com>
2012-03-11 11:58:29 +0000

    feed works, designed for small numbers of following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,9 @@ class Micropost < ActiveRecord::Base
 	validates :user_id, :presence => true
 
 	default_scope :order => 'microposts.created_at DESC'
+
+  def self.from_users_followed_by(user)
+    following_ids = user.following_ids
+    where("user_id IN (?) OR user_id = ?", following_ids, user)
+  end
 end
